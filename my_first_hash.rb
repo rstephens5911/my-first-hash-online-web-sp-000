c0f2fcf66271e2e3b77ae76f808da778a240f551
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-31 14:39:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {"dog" => "Scotty", "cat" => "Rollie"}
+  my_dogs = {"dog1" => "Scotty", "dog2" => "Penny", "dog3" => "Chase", "dog4" => "Dolly"}
 end
 
 
@@ -10,40 +10,24 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   # manifest guide:
   # {inventory item => quantity}
-  the_manifest = {
-    "whale bone corsets" => 5,
-    "porcelain vases" => 2,
-    "oil paintings" => 3
-  }
-end
+
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifest["oil paintings"]
+
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest ["muskets"] = 2
 
+  # add 2 muskets to the shipping_manifest hash below
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] = 4
+
 
 
   # return the shipping_manifest hash below
-  shipping_manifest
+
 
 end
