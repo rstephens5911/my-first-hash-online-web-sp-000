b659a659adff2c838c99c5163e9cf9fa6a88a2f0
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-31 14:51:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,5 +35,5 @@ def adding
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
-  shipping_manifest
+  return shipping_manifest
 end
